b885c72a03c6411efa82236bdf2f4d9a5517d77e
Giada196 <jadeinwonderland22@gmail.com>
2024-05-07 19:15:49 +0200

    Esercizio Spring Boot
    "scrivi una applicazione web Spring Boot con:
    NameController dove si mappa il parametro name per:
    restituire il nome alla chiamata GET
    restuiture il nome al contrario (es. da John a nhoJ, usando StringBuilder) alla chiamata POST
    testare le chiamate del API endpoint usando Postman"
    
    Chiamata API:
    GET: http://localhost:8080/v1/nome?name=John
    POST: http://localhost:8080/v1/reverse?name=John

---
+++ demo/src/main/java/com/example/demo/NameController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1")
+
+public class NameController {
+    @GetMapping("/nome")
+    public String nome (@RequestParam( required = true) String name) {
+        return name;
+    }
+    @PostMapping("/reverse")
+    public String reverse(@RequestParam String name){
+        StringBuilder reversename = new StringBuilder(name).reverse();
+        return reversename.toString();
+    }
+
+}
