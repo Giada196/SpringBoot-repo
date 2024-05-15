0c156efc8208d89a4d387c00b4f7c2bcef9dc1b7
Giada196 <jadeinwonderland22@gmail.com>
2024-05-15 14:40:14 +0200

    Aggiunta documentazione

--- demo-swagger-example2/src/main/java/com/example/demoswagger/example2/NomeController.java
+++ demo-swagger-example2/src/main/java/com/example/demoswagger/example2/NomeController.java
@@ -1,6 +1,8 @@
 package com.example.demoswagger.example2;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,13 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
     public class NomeController {
-        @GetMapping("/nome")
-        public String nome (@RequestParam( required = true) String name) {
-            return name;
-        }
+    @Operation(summary = "getNome", description = "restituisce il nome (name) fornito")
+    @GetMapping("/nome")
+    public String nome(
+        @Parameter(description = "nome (name) da restituire") @RequestParam String name){
+        return name;
+    }
+    @Operation(summary = "reverse", description = "restituisce il nome (name) al contrario")
         @PostMapping("/reverse")
-        public String reverse(@RequestParam String name){
-            StringBuilder reversename = new StringBuilder(name).reverse();
-            return reversename.toString();
-        }
-}
+        public String reverse (
+            @Parameter(description = "nome (name) ritorna al contrario") @RequestParam String name){
+        StringBuilder reversename = new StringBuilder(name).reverse();
+        return reversename.toString();
+    }
+    }
