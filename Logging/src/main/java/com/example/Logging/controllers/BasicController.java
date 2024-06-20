8152b3e4356cf8befd862fed6e23aeadfbaee497
Giada196 <jadeinwonderland22@gmail.com>
2024-06-20 12:54:40 +0200

    Esercizi Spring Boot
    Esercizio Spring Boot Logging

---
+++ Logging/src/main/java/com/example/Logging/controllers/BasicController.java
@@ -0,0 +1,41 @@
+package com.example.Logging.controllers;
+
+
+import com.example.Logging.services.BasicService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping
+public class BasicController {
+
+    Logger logger = LoggerFactory.getLogger(BasicController.class);
+    private final BasicService basicService;
+
+    public BasicController(BasicService basicService) {
+        this.basicService = basicService;
+    }
+
+    @GetMapping
+    public String welcome() {
+        logger.info("Accessed welcome endpoint");
+        return "Welcome to the Spring Boot Application!";
+    }
+
+    @GetMapping("/exp")
+    public double exp() {
+        logger.debug("Finished exponentiation calculation");
+        return basicService.calculateExponentiation();
+    }
+
+    @GetMapping("/get-errors")
+    public void getErrors() {
+        logger.error("Custom error endpoint triggered");
+    }
+}
+
